987fc9cc2c324d6d30e382c23b94f1c93964e004
frostgareline <rioyoyam25@gmail.com>
2025-04-26 01:30:55 +0700

    Add Vercel Analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { AuthProvider } from '../contexts/AuthContext';
+import { Analytics } from "@vercel/analytics/react";
 // import VidsrcFixer from "./components/VidsrcFixer";
 
 const geistSans = Geist({
@@ -70,6 +71,7 @@ export default function RootLayout({
             </div>
           </div>
         </AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
